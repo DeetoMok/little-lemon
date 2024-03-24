79d68fa41a3ee840c74f3720058d511c91c28976
Daryl <darylmok1@gmail.com>
2024-03-24 18:19:44 +0800

    change js to tsx

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import Header from './Components/Header';
+import Nav from './Components/Nav';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Nav/>
+      <Main/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
